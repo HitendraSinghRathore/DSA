7171620e2fe5e3df78475f6c520a37ffba4a8a82
hitendrarathore <hitendrarathore@gofynd.com>
2025-04-24 09:07:45 +0530

    update git

---
+++ arraydivisble3.js
@@ -0,0 +1,22 @@
+function arrayCheck(arr) {
+    const mod =  {0 : -1};
+    let maxLen = 0, prefixSum = 0;
+
+    for(let i=0;i< arr.length;i++) {
+        prefixSum += arr[i];
+        const modVal = prefixSum % 3;
+
+        if(modVal in  mod) {
+            maxLen = Math.max(maxLen, i - mod[modVal]);
+        }  else {
+            mod[modVal] = i;
+        }
+        
+    }
+    return maxLen;
+
+}
+
+
+
+console.log(arrayCheck([1, 2, 3, 4, 5,7,5,3,1,3,5,6,7,3,2,4,5,2]));
